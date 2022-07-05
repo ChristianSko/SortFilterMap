19d71388e3dd6b631e0b78abeb2a28b9d2da2a2c
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-05 08:39:52 +0200

    Initial Commit

---
+++ SortFilterMap/SortFilterMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortFilterMapApp.swift
+//  SortFilterMap
+//
+//  Created by Skorobogatow, Christian on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SortFilterMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
